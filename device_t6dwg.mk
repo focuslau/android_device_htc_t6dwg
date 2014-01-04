ab0f2ec917aef31460f47b2b2a6378bf135fc398
focuslau <focus.lau@gmail.com>
2014-01-04 10:26:07 +0800

    vold.fstab is no longer used and has been integrated in fstab.[DEVICE]

--- device_t6dwg.mk
+++ device_t6dwg.mk
@@ -23,10 +23,6 @@ endif
 PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
 
-# vold config
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/vold.fstab:system/etc/vold.fstab
-
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=8
